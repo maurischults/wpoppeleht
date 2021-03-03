dcbaa4e7b9a9306ce8152137b25cb661dcca009f
maurischultsiktk <mauri.schults@khk.ee>
2021-03-03 13:02:34 +0200

    deleted plugin `Valentine's Day Hearts` version 2.0 (after activation of `Valentine's Day Hearts` version 2.0)

--- wp-content/plugins/uusplugin/my-first-plugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: EsimenePlugin
-Plugin URI: https://maurischults.ikt.khk.ee/
-Description: This is my first WordPress Plugin
-Author: Mauri Schults
-Author URI: https://maurischults.ikt.khk.ee/
-Version: 1.0
-*/
