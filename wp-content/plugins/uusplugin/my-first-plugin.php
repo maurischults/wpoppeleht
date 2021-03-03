0e60cefc293d642cb614e171e8d5a491445fde0f
maurischultsiktk <mauri.schults@khk.ee>
2021-03-03 12:51:19 +0200

    modified plugin `EsimenePlugin` version 1.0 (after activation of `EsimenePlugin` version 1.0)

---
+++ wp-content/plugins/uusplugin/my-first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: EsimenePlugin
+Plugin URI: https://maurischults.ikt.khk.ee/
+Description: This is my first WordPress Plugin
+Author: Mauri Schults
+Author URI: https://maurischults.ikt.khk.ee/
+Version: 1.0
+*/
